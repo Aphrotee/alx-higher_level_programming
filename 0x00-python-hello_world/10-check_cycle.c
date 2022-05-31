c42aec29c557d5376b4b2fda74fe8b8223bb25b7
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-30 18:58:38 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,6 +2,25 @@
 #include <stddef.h>
 
 /**
+ * _check - checks if it is a repeated pointer
+ * @head: array of pointers
+ * @temp: current pointer
+ *
+ * Return: 0 or 1
+ */
+int _check(listint_t *head[], listint_t *temp)
+{
+	int i = 0;
+
+	while (head[i] != NULL)
+	{
+		if (head[i] == temp)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+/**
  * check_cycle - checks if there is a cycle in the linked list
  * @list: linked list
  *
@@ -9,15 +28,20 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *temp, *head;
+	listint_t *temp, *head[] = {NULL};
+	int j = 0;
 
-	head = list;
 	temp = list;
+	head[0] = list;
+	head[1] = NULL;
 	while (temp != NULL)
 	{
-		if (temp->next == head)
+		if (_check(head, temp))
 			return (1);
+		j++;
 		temp = temp->next;
+		head[j] = temp;
+		head[j + 1] = NULL;
 	}
 	return (0);
 }
