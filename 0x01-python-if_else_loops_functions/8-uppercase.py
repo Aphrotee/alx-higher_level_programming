664c26b47927536367533b4dfd552b23d735f663
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-01 03:16:24 +0100

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
 def uppercase(sstr):
-    c = len(sstr)
-    if c == 0:
-        return
-    if c != 1:
-        for a in range(0, c-1):
+    c = len(sstr)        
+    if (c != 1) and (c != 0):
+        for a in range(0, c):
             b = ord(sstr[a])
             if (b >= 97) and (b <= 122):
                 b = b - 32
             print("{}".format(chr(b)), end="")
-    b = ord(sstr[c-1])
-    if (b >= 97) and (b <= 122):
-        b -= 32
-    print("{}".format(chr(b)))
+    print("")
