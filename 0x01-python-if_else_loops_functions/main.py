bcda515789312ab038d29d1c5586d5c6c9403d8f
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-31 17:43:59 -0700

    task 7

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -7,9 +7,3 @@ print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
 
-guillaume@ubuntu:~/0x01$ ./7-main.py
-a is lower
-H is upper
-A is upper
-3 is upper
-g is lower
