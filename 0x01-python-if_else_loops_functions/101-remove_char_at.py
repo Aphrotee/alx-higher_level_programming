a3dad4a1c6d042f3ec61415566e6f7974e132542
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-01 08:22:08 -0700

    task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,13 +4,15 @@ def remove_char_at(str, n):
     if (str == ""):
         return ""
     cp = str
+    if (n < 0):
+        return cp
     if (n == 0):
         cp = cp[1:]
-    elif (n ==  -1):
+    elif (n == -1):
         cp = cp[:-1]
     else:
         cp1 = cp[:n]
-        cp2 = cp[n+1:]
-        cp = cp1 +cp2
+        cp2 = cp[n + 1:]
+        cp = cp1 + cp2
     return cp
 
