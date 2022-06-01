663f47e78da144bf781f8de925e333a5c54585c1
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-31 18:12:47 -0700

    tasks 7 & 8

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 
 def islower(alph):
     i = ord(alph)
-    if (i >= 97) and (i <= 122 ):
+    if (i >= 97) and (i <= 122):
         return True
     elif (i >= 65) and (i <= 90):
         return False
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uppercase(sstr):
+    for a in range(0, len(sstr)-1):
+        b = ord(sstr[a])
+        if (b >= 97) and (b <= 122):
+            b = b - 32
+        print(chr(b), end="")
+    b = ord(sstr[a+1])
+    if (b >= 97) and (b <= 122):
+        b -= 32
+    print(chr(b) + "\n")
