372cecbeb87b46887376c3eacda750721cdadffd
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-01 03:20:08 +0100

    tasks 11 & 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for i in range(1, 61):
         if (i % 3 == 0) and (i % 5 == 0):
             print("Fizzbuzz ", end="")
-        elif (i % 3):
+        elif (i % 3 == 0):
             print("Fizz ", end="")
-        elif (i % 5):
+        elif (i % 5 == 0):
             print("Buzz ", end="")
