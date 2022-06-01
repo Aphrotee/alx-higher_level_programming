8fdfba2ba8b9a4b45f124f73006546b3ba037ce4
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-05-31 18:34:54 -0700

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 def uppercase(sstr):
-    for a in range(0, len(sstr)-1):
-        b = ord(sstr[a])
-        if (b >= 97) and (b <= 122):
-            b = b - 32
-        print("{}".format(chr(b)), end="")
-    b = ord(sstr[a+1])
+    c = len(sstr)
+    if c == 0:
+        return
+    if c != 1:
+        for a in range(0, c-1):
+            b = ord(sstr[a])
+            if (b >= 97) and (b <= 122):
+                b = b - 32
+            print("{}".format(chr(b)), end="")
+    b = ord(sstr[c-1])
     if (b >= 97) and (b <= 122):
-        b -= 32
+       b -= 32
     print("{}".format(chr(b)))
