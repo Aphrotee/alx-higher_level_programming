b7069df3d714a4eb89bd8ddba723565a8558fac3
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-02 18:39:31 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,11 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-c = str(a)
-d = str(b)
 u = str(add(a, b))
 v = str(sub(a, b))
 w = str(mul(a, b))
 x = str(div(a, b))
+a = str(a)
+b = str(b)
 print("{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}\
-        ".format(c, d, u, c, d, v, c, d, w, c, d, x))
+        ".format(a, b, u, a, b, v, a, b, w, a, b, x))
