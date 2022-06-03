b7de8cc63373082a15b765dd245e788108be4f5c
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-03 04:15:17 +0100

    task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    u = str(add(a, b))
+    u = add(a, b)
+    print("{} + {} = {}".format(a, b, u))
     v = str(sub(a, b))
+    print("{} - {} = {}".format(a, b, v))
     w = str(mul(a, b))
+    print("{} * {} = {}".format(a, b, w))
     x = str(div(a, b))
-    print("{} + {} = {}\n{} - {} = {}\n{} * {} = {}\n{} / {} = {}\
-        ".format(a, b, u, a, b, v, a, b, w, a, b, x))
+    print("{} / {} = {}".format(a, b, x))
