1ceba2ed3da46453c558e866f6187355a9e5d3c7
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-04 09:45:40 -0700

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("$")
+        return
+    for i in matrix:
+        a = len(matrix)
+        for j in range(a):
+            if j != a - 1:
+                print("{:d} ".format(i[j]), end="")
+            else:
+                print("{:d}$".format(i[j]))
