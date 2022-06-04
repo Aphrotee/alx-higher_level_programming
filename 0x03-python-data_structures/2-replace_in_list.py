0ed000620d4f38280bae619c4dd99dbe238648c3
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-04 14:42:23 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if (idx >= 0) and (idx < len(my_list)):
+    idxx = str(idx)
+    if (idx >= 0) and (idx < len(my_list) and idx.isdigit()):
         my_list[idx] = element
         return my_list
