7a4265192ddedb535ed4385c758d5e57bb524252
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-06 08:10:40 -0700

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     leng = len(my_list)
     new = my_list
-    if leng == 0 or idx >= leng or idx < 0 :
+    if leng == 0 or idx >= leng or idx < 0:
         return my_list
     else:
         rem = new[idx]
