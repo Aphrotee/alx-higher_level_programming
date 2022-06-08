0428c543920881e828a7242684b80b62862b386c
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-08 10:44:29 -0700

    tasks 0, 1, 2 & 3

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list == []:
+        return my_list
     replaced = my_list[:]
     for i in range(len(replaced)):
         if replaced[i] == search:
