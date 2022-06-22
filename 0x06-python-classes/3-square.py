5aea615ad6a3939ea55312f7890212396e068064
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-22 06:45:05 -0700

    task 5

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -27,6 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
         """
         area: returns area of square
