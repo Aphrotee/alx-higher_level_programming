3148f9bec27d5f669af08c9c5967ddc95eef6426
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-06-23 02:27:23 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,4 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+
+"""
+This is a module that provides a MagicClass Object for a
+Circle which has a private instance attribute "radius"
+which must either be float or integer.
+This class has a method to calculate the area of the circle
+and a method to calculate the circumference of the circle
+"""
 
 import dis
 import math
@@ -6,16 +14,27 @@ import math
 
 class MagicClass:
 
+    """
+    This is a class Object for a
+    Circle which has a private instance attribute "radius"
+    which must either be float or integer.
+    This class has a method to calculate the area of the circle
+    and a method to calculate the circumference of the circle
+    """
 
-     def __init__(self, radius=0):
+    def __init__(self, radius):
         if type(radius) != float and type(radius) != int:
             raise TypeError("radius must  be a number")
         else:
             self.__radius = radius
 
+    @property
+    def radius(self):
+        return self.__radius
 
     def area(self):
         return (math.pi * (self.__radius ** 2))
 
     def circumference(self):
         return (2 * math.pi * self.__radius)
+
