f67424d78c61cf9b1f1998d436572bc0ae4f6441
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-06-29 20:15:07 -0700

    all tasks

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,16 +6,9 @@ This is the module that supplies the class LockedClass
 
 class LockedClass:
     """
-    Class for initializing LockedClass object
+    Class for initializing LockedClass object.
+    It prevents the user from dynamically creating new
+    instance attributes, except if the new
+    instance attribute is called first_name.
     """
-
-    def __init__(self):
-        self.first_name = ""
-
-    def __setattr__(self, key, value):
-        if not hasattr(self, key):
-            raise AttributeError("'LockedClass' \
-object has no attribute 'last_name'")
-        else:
-            self.__first_name = value
-        object.__setattr__(self, key, value)
+    __slots__ = ['first_name']
