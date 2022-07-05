7eda24a8310211159b17420b69400f49d1f360a9
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-05 05:53:35 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,7 +10,7 @@ class MyInt(int):
     """
 
     def __eq__(self, other):
-        return self- other != 0
+        return self - other != 0
 
     def __ne__(self, other):
         return self - other == 0
