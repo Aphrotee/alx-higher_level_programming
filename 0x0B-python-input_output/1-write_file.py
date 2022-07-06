9079a6457081f16809bd4870aa661bea012c0297
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-06 05:44:44 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,9 @@ This module supplies the function "write_file".
 
 
 def write_file(filename="", text=""):
+    """
+    This function writes to a file.
+    """
     with open(filename, mode="w", encoding="UTF-8") as fd:
         a = fd.write(text)
     return a
