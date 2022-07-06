aeb155ca3183124eb7aaab3b88bda6413832f56b
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-06 05:39:24 +0100

    correct error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ This module supplies the function "write_file".
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding="UTF-8") as fd:
+    with open(filename, mode="w", encoding="UTF-8") as fd:
         a = fd.write(text)
     return a
