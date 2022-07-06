aa7af5c4fb1f409e2a15ee817f6b60659af8e167
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-06 05:31:41 +0100

    tasks 0 through 11

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,9 +9,8 @@ def read_file(filename=""):
     This function reads a file and prints it to the stdout.
     """
 
-    if len(filename) == 0:
-        return
+
     with open(filename, 'r', encoding="UTF-8") as fd:
-        filee = fd.readlines()
-        for a in range(len(filee)):
-            print(filee[a], end='')
+
+        for a in fd:
+            print(a, end='')
