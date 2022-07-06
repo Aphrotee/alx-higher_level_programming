e24cf2c50da2cd6d7139cc69f0be5cb7d7ce7337
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-06 15:25:48 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,9 +4,6 @@ This is the module that supplies the function "class_to_json".
 """
 
 
-import json
-
-
 def class_to_json(obj):
     """
     This is a function that returns the dictionary description
