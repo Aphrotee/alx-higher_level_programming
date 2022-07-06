6692ed3edfe843e8db03bdf94e77bb7104b4f887
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-07-06 05:45:49 +0100

    tasks 0 through 11

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,9 @@ This module supplies the function "append_write".
 
 
 def append_write(filename="", text=""):
+    """
+    This function appends to a file
+    """
     with open(filename, 'a', encoding="UTF-8") as fd:
         a = fd.write(text)
     return a
