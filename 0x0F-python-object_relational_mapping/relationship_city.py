f3d01e9f97c4a67c53f24796a1ad23e00731f98e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-10 22:38:26 +0000

    mandatory tasks

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,24 +2,25 @@
 
 """
  a python file that contains the
-class definition of a State and
+class definition of a City and
 an instance Base = declarative_base()
 """
 
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """
     This class inherits from Base
-    and it links to the MySQL table states.
+    and it links to the MySQL table cities.
     """
 
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True,
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -7,8 +7,10 @@ an instance Base = declarative_base()
 """
 
 
+from relationship_city import City
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -23,3 +25,4 @@ class State(Base):
     id = Column(Integer, primary_key=True,
                 nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
