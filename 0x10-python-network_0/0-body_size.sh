9000684c734624fc6b43c94c667cc04718bb54a4
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-22 19:30:15 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # A Bash script that takes in a URL, sends a
 # request to that URL, and displays the size
 # of the body of the response.
-curl -Is "$1" | grep "Content-Length" | tr " " "\n" | tail -1
+curl -Is $1" | grep "Content-Length" | tr " " "\n" | tail -1
