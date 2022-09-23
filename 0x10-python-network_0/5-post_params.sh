11157166053e3c117809eb5c0cff408f7444d60e
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-22 19:29:10 -0700

    all tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This is a  Bash script that takes in a URL, sends a POST
 # request to the passed URL, and displays the body of the response
- curl -X POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
+curl -X POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
