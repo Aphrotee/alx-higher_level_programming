3a41478d72a438e23555c2fbc3c93b22c4b6d667
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-24 00:14:59 +0100

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,9 +7,15 @@ to the URL and displays the body of the response (decoded in utf-8).
 
 import urllib
 import sys
+from urllib.error import HTTPError
 import urllib.parse
 import urllib.request
 
 
 if '__name__' == '__main__':
-    
+    req = urllib.request.Request(sys.argv[1], data)
+    try:
+        response = urllib.request.urlopen(req)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
+        
