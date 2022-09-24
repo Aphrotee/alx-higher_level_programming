42079212881edf258489d0da5819b052e794bcef
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-09-24 04:54:14 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("    - type: {}".format(type(r.content.decode('utf-8'))))
-    print("    - content: {}".format(r.content.decode('utf-8')))
+    print("\t- type: {}".format(type(r.content.decode('utf-8'))))
+    print("\t- content: {}".format(r.content.decode('utf-8')))
