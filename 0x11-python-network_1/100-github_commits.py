19d335f883964873ba4ce6bb55e94538ce151a79
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-24 03:55:28 +0100

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,4 +9,5 @@ import requests
 if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/{}/{}/commits'.format(sys.argv[1], sys.argv[2]), data={'page': 10})
     js = r.json()
-    print(js)
+    for i in range(10):
+        print("{}: {}".format(js[i].get('sha'), js[i].get('commit').get('author').get('name')))
