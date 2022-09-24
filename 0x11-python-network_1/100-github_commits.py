bf8165f356291bd7856ffcbf1d933ed1f2389aa0
Aphrotee <94745676+Aphrotee@users.noreply.github.com>
2022-09-24 05:34:06 +0100

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,9 +8,9 @@ import sys
 import requests
 if __name__ == '__main__':
     url = 'https://api.github.com/repos/'
-    r = requests.get(url + '{}/{}/commits'.format(
+    r = requests.get(url + '{}/{}/commits?per_page=10'.format(
                                     sys.argv[1],
-                                    sys.argv[2]), data={'per_page': 10})
+                                    sys.argv[2]))
     js = r.json()
     for i in range(10):
         print("{}: {}".format(js[i].get('sha'),
