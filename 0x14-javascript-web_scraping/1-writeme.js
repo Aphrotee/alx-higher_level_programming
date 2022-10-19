25e6969b3f9342790176db62c98c646c6ba60ca0
Aiyeboan Temitope <aphrotemitope37@gmail.com>
2022-10-18 20:20:20 -0700

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// This is a script that writes to a file.
+
+const fs = require('fs');
+const cmd = process.argv.slice(2);
+const filename = cmd[0];
+const cont = cmd[1] + '\n';
+fs.writeFile(filename, cont, (err) => {
+  if (err) console.log(err);
+});
