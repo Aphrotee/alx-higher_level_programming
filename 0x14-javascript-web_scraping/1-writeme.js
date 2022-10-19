2bc1c63a8dc7894bc6821182406508a08d5e5d0b
Aiyeboan Temitope <aphrotemitope37@gmail.com>
2022-10-18 20:22:08 -0700

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@
 const fs = require('fs');
 const cmd = process.argv.slice(2);
 const filename = cmd[0];
-const cont = cmd[1] + '\n';
+const cont = cmd[1];
 fs.writeFile(filename, cont, (err) => {
   if (err) console.log(err);
 });
